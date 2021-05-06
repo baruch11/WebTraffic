3a14a77ad00510110f900e2bad83e2f474b7842f
C.H. Prat <charles-henri.prat@parrot.com>
2021-05-06 14:10:23 +0200

    don't use tf.dataset anymore

--- webtraffic_utils.py
+++ webtraffic_utils.py
@@ -7,31 +7,57 @@ import matplotlib.pyplot as plt
 
 class OneHotEncodingLayer(tf.keras.layers.experimental.preprocessing.PreprocessingLayer):
 
-    def __init__(self, vocabulary=None, depth=None):
-        super().__init__()
+    def __init__(self, vocabulary=None, depth=None, **kwargs):
+        super().__init__(**kwargs)
         self.vocabulary = vocabulary
         indices = tf.range(len(vocabulary), dtype=tf.int64)
         table_init = tf.lookup.KeyValueTensorInitializer(vocabulary, indices)
         self.table_ohe = tf.lookup.StaticVocabularyTable(table_init, num_oov_buckets=1)
         self.depth = depth
         if depth is None:
-            self.depth = tf.cast(self.table_ohe.size(), tf.int32)
+            self.depth = self.table_ohe.size()
 
     def call(self,inputs):
-        return  tf.one_hot(self.table_ohe.lookup(inputs), depth=self.depth)
+        return  tf.one_hot(self.table_ohe.lookup(inputs), depth=tf.cast(self.depth, tf.int32))
 
     def get_config(self):
-        return {'vocabulary': self.vocabulary, 'depth': self.depth}
+        return {'vocabulary': list(self.vocabulary), 'depth': int(self.depth)}
 
 
 def smape(A, F):
     return tf.reduce_mean(2 * tf.math.abs(F - A) / (tf.math.abs(A) + tf.math.abs(F) + 1e-16)) * 100
 
 
-def smape_reg(A, F):
+def smape_reg(ypred, ytrue):
+    A = tf.cast(ypred, tf.float32)
+    F = tf.cast(ytrue, tf.float32)
     epsilon = 0.1
     summ = tf.maximum(tf.abs(A) + tf.abs(F) + epsilon, 0.5 + epsilon)
-    return tf.abs(A - F) / summ * 2.0 * 100
+    return  tf.reduce_mean( 2* tf.abs(A - F) / summ) * 100
+
+
+class SmapeMetric(tf.keras.losses.Loss):
+    def __init__(self, name="smape", **kwargs):
+        super().__init__(name=name, **kwargs)
+
+    def call(self, y_true, y_pred):
+        return smape(y_pred, y_true)
+
+    def get_config(self):
+        base_config = super().get_config()
+        return {**base_config}
+
+
+class SmapeLoss(tf.keras.losses.Loss):
+    def __init__(self, name="smape_reg", **kwargs):
+        super().__init__(name=name, **kwargs)
+
+    def call(self, y_true, y_pred):
+        return smape_reg(y_pred, y_true)
+
+    def get_config(self):
+        base_config = super().get_config()
+        return {**base_config}
 
 
 def smape_np(A, F):
@@ -78,20 +104,32 @@ def ds_from_dataframe(df):
 
 
 
-def plot_check_result(df, page, model):
+def get_model_inputs(df_augmented):
+    feat_cols = [ii for ii in df_augmented if "feat_" in ii]
+    df_train = df_augmented.drop(columns=feat_cols)
+    inputs = [tf.convert_to_tensor(df_train.index), df_train.values[:,:-62]]
+    for ii in feat_cols:
+        inputs.append(tf.convert_to_tensor(df_augmented[ii].values))
+    return inputs, df_train.values[:,-62:]
+
+
+def plot_check_result(df_train, page, models):
     """ df: dataframe from original csv file """
-    lds = ds_from_dataframe(df.loc[df["Page"] == page])
-    smape_score = model.evaluate(lds.batch(1), verbose=0)[1]
+    features, target = get_model_inputs(df_train.loc[[page]])
+    f, vax = plt.subplots(len(models),2, figsize=(10,4*len(models)))
+    fax = vax.flat
 
-    features, target = list(lds.take(1))[0]
-    pred = model.predict(lds.batch(1), verbose=0)[0]
+    for model in models:
+        smape_score = model.evaluate(features, target, verbose=0)[1]
+        pred = model.predict(features, verbose=0)[0]
 
-    f, (ax, ax1) = plt.subplots(1,2, figsize=(10,4))
-    ax.set_title("smape: {:.2f}".format(smape_score))
-    ax.plot(target, label="pred")
-    ax.plot(pred, label="prediction")
-    ax1.plot(np.r_[features[1],target])
-    ax1.plot(np.arange(0,len(pred))+len(features[1]),pred)
+        ax= next(fax)
+        ax.set_title("smape: {:.2f}".format(smape_score))
+        ax.plot(target.reshape(-1,), label="pred")
+        ax.plot(pred.reshape(-1,), label="prediction")
+        ax1= next(fax)
+        ax1.plot(df_train.loc[page].values)
+        ax1.plot(np.arange(0,len(pred))+len(features[1].reshape(-1,1)),pred)
 
     f.suptitle(page)
     plt.show()
