8623c610a4fa3582c78b1f982bd20996a1f3b447
CHARLES-HENRI PRAT <charprat@yahoo.fr>
2023-03-17 17:40:56 +0100

    add class to help submission

--- webtraffic/inout.py
+++ webtraffic/inout.py
@@ -2,11 +2,19 @@
 import os
 import pandas as pd
 import numpy as np
+import logging
+from dataclasses import dataclass
 
 TRAIN_FIRST_DAY = '2015-07-01'
 TRAIN_LAST_DAY = '2017-09-10'
 PRED_FIRST_DAY = '2017-09-13'
 PRED_LAST_DAY = '2017-11-13'
+
+
+def get_root_dir() -> str:
+    """Return project root directory path."""
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(this_dir, "..")
 
 
 def load_data(nsamples=None):
@@ -22,9 +30,8 @@ def load_data(nsamples=None):
     traffic_ds : (pd.DataFrame)
         index: site, columns: days, data: page hits
     """
-    this_dir = os.path.dirname(os.path.realpath(__file__))
     datapath = "data/web-traffic-time-series-forecasting/train_2.csv.zip"
-    traffic_ds = pd.read_csv(os.path.join(this_dir, "..", datapath))
+    traffic_ds = pd.read_csv(os.path.join(get_root_dir(), datapath))
     traffic_ds = traffic_ds.set_index("Page").fillna(0).astype(np.int32)
     if nsamples is not None:
         traffic_ds = traffic_ds.sample(nsamples)
@@ -62,3 +69,39 @@ def _get_forecast_horizon():
     """Return forecast horizon (how many days to predict)."""
     return (pd.Timestamp(PRED_LAST_DAY) -
             pd.Timestamp(PRED_FIRST_DAY)).days + 1
+
+
+@dataclass
+class Submission:
+    """This class represents the kaggle submission.
+
+    After exporting in a csv file (let's say subm_median.csv), submit
+    the result with the following command:
+      kaggle competitions submit -f subm_med.csv -m 'median model' web-traffic-time-series-forecasting
+    """
+
+    keys_path: str
+    predictions: np.array
+    index: pd.Index
+    start_date: str = PRED_FIRST_DAY
+    end_date: str = PRED_LAST_DAY
+
+    def to_csv(self, output_path: str):
+        """Write a csv file with the kaggle submission."""
+        logging.info("Loading output keys")
+        submission = pd.read_csv(self.keys_path).set_index("Page")
+        logging.info("Exporting submission in %s", output_path)
+        predictions = self._serialize_predictions()
+        submission.loc[predictions.index, "Visits"] = predictions
+        submission.to_csv(output_path, encoding='utf-8', index=False)
+
+    def _serialize_predictions(self):
+        """Return a pd.Series with predictions indexed like key2.csv."""
+        out_date = pd.date_range(start=self.start_date,
+                                 end=self.end_date,
+                                 freq="1D").strftime("%Y-%m-%d").to_list()
+        ret = pd.DataFrame(self.predictions,
+                           columns=out_date,
+                           index=self.index).stack().rename("Visits")
+        ret.index = [ii[0]+"_"+ii[1] for ii in ret.index]
+        return ret
--- webtraffic/train.py
+++ webtraffic/train.py
@@ -1,8 +1,12 @@
 """Main script to train and make submission."""
 import argparse
 import pandas as pd
+import os
+import logging
 
-from webtraffic.inout import load_data, get_training_datasets
+
+from webtraffic.inout import (load_data, get_training_datasets, Submission,
+                              get_root_dir)
 from webtraffic.models.median_model import median_model
 from webtraffic.webtraffic_utils import smape_np
 
@@ -14,6 +18,8 @@ parser.add_argument('--nsamp', '-n', type=int, default=None,
                     "(all samples if None (default))")
 
 args = parser.parse_args()
+
+logging.getLogger().setLevel(logging.INFO)
 
 print("loading traffic dataset.")
 traffic = load_data(args.nsamp)
@@ -33,3 +39,19 @@ test_pred = model.predict(X_test.values)
 print(pd.Series(index=["train", "test"],
                 data=[smape_np(train_pred, Y_train.values),
                       smape_np(test_pred, Y_test.values)]))
+
+# blind retraining on the last datas ?
+
+
+# exporting the predictions
+print("Predicting for submission")
+final_preds = model.predict(traffic.values[:, -X_train.shape[1]:])
+
+keys_path = os.path.join(get_root_dir(),
+                         "data/web-traffic-time-series-forecasting",
+                         "key_2.csv.zip")
+
+forecast_sub = Submission(keys_path,
+                          final_preds,
+                          traffic.index)
+forecast_sub.to_csv("subm_med.csv")
